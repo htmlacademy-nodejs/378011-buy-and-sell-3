721a6a8e56257cdc6ead7ded72ef8e9d389c0d7c
Lana Kostina <exxlain@gmail.com>
2020-02-01 22:27:59 +0700

    added server command

--- src/service/cli/constants.js
+++ src/service/cli/constants.js
@@ -2,7 +2,7 @@
 
 const DEFAULT_COUNT = 1;
 
-const FILE_NAME = `mock.json`;
+const FILE_NAME = `mocks.json`;
 
 const HELP = `Программа запускает http-сервер и формирует файл с данными для API.
 
@@ -47,12 +47,29 @@ const Messages = {
   READING_ERROR: `Can't read data from file`,
   SUCCESS: `Operation success. File created.`,
   OVERMUCH: `Не больше 1000 объявлений`,
+  NOT_FOUND: `Not found`,
+  SERVER_CREATION_ERROR: `Ошибка при создании сервера`,
+  WAITING_CONNECTION: `Ожидаю соединений на`,
 };
+
 const GENERATE_COMMAND = `--generate`;
 
 const FILE_SENTENCES_PATH = `./data/sentences.txt`;
 const FILE_TITLES_PATH = `./data/titles.txt`;
 const FILE_CATEGORIES_PATH = `./data/categories.txt`;
+
+const DEFAULT_PORT = 3000;
+const FILENAME = `mocks.json`;
+
+const ROOT_PAGE_PATH = `/`;
+
+const HttpCode = {
+  OK: 200,
+  NOT_FOUND: 404,
+  INTERNAL_SERVER_ERROR: 500,
+  FORBIDDEN: 403,
+  UNAUTHORIZED: 401,
+};
 
 module.exports = {
   DEFAULT_COUNT,
@@ -71,4 +88,8 @@ module.exports = {
   FILE_SENTENCES_PATH,
   FILE_TITLES_PATH,
   FILE_CATEGORIES_PATH,
+  DEFAULT_PORT,
+  FILENAME,
+  HttpCode,
+  ROOT_PAGE_PATH,
 };
