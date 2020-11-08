99743b840209a82f9a803b3ebae3b806584768ec
Svetlana Kostina <kostina@sibext.com>
2020-11-08 21:27:36 +0700

    [added] axios and api requests

--- src/express/routes/main.js
+++ src/express/routes/main.js
@@ -2,10 +2,27 @@
 
 const {Router} = require(`express`);
 const mainRouter = new Router();
+const api = require(`./../api`).getAPI();
 
-mainRouter.get(`/`, (req, res) => res.render(`main/main`));
+mainRouter.get(`/`, async (req, res) => {
+  const offers = await api.getOffers();
+  res.render(`main/main`, {offers});
+});
 mainRouter.get(`/register`, (req, res) => res.render(`main/sign-up`));
 mainRouter.get(`/login`, (req, res) => res.render(`main/login`));
-mainRouter.get(`/search`, (req, res) => res.render(`main/search-result`));
+mainRouter.get(`/search`, async (req, res) => {
+  try {
+    const {search} = req.query;
+    const results = await api.search(search);
+
+    res.render(`main/search-result`, {
+      results
+    });
+  } catch (error) {
+    res.render(`main/search-result`, {
+      results: []
+    });
+  }
+});
 
 module.exports = mainRouter;
--- src/express/routes/offers.js
+++ src/express/routes/offers.js
@@ -2,10 +2,18 @@
 
 const {Router} = require(`express`);
 const offersRouter = new Router();
+const api = require(`./../api`).getAPI();
 
 offersRouter.get(`/category/:id`, (req, res) => res.render(`offers/category`));
 offersRouter.get(`/add`, (req, res) => res.render(`offers/new-ticket`));
-offersRouter.get(`/edit/:id`, (req, res) => res.render(`offers/ticket-edit`));
 offersRouter.get(`/:id`, (req, res) => res.render(`offers/ticket`));
+offersRouter.get(`/edit/:id`, async (req, res) => {
+  const {id} = req.params;
+  const [offer, categories] = await Promise.all([
+    api.getOffer(id),
+    api.getCategories()
+  ]);
+  res.render(`offers/ticket-edit`, {offer, categories});
+});
 
 module.exports = offersRouter;
