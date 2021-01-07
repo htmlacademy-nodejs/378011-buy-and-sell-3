5f27bfda4bfe81ff3fe5bb6de64ec41fff4b0c9e
Svetlana Kostina <kostina@sibext.com>
2021-01-07 15:58:29 +0700

    [added] schema

---
+++ schema.sql
@@ -0,0 +1,110 @@
+DROP DATABASE IF EXISTS academy_buy_and_sell;
+DROP ROLE IF EXISTS academy;
+
+CREATE ROLE academy WITH
+	LOGIN
+	NOSUPERUSER
+	NOCREATEDB
+	NOCREATEROLE
+	INHERIT
+	NOREPLICATION
+	CONNECTION LIMIT -1
+	PASSWORD 'academy';
+
+
+CREATE DATABASE academy_buy_and_sell
+    WITH
+    OWNER = academy
+    TEMPLATE = template0
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'C'
+    LC_CTYPE = 'C'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+
+DROP TABLE IF EXISTS offers_categories;
+DROP TABLE IF EXISTS offes_types;
+DROP TABLE IF EXISTS types;
+DROP TABLE IF EXISTS categories;
+DROP TABLE IF EXISTS comments;
+DROP TABLE IF EXISTS offers;
+DROP TABLE IF EXISTS users;
+
+CREATE TABLE users
+(
+  id BIGSERIAL PRIMARY KEY,
+  first_name VARCHAR(100) NOT NULL,
+  last_name VARCHAR(100) NOT NULL,
+  email VARCHAR(100) UNIQUE NOT NULL,
+  password VARCHAR(100) NOT NULL,
+  avatar TEXT
+);
+
+CREATE TABLE offers
+(
+    id BIGSERIAL PRIMARY KEY,
+    title VARCHAR(100) NOT NULL,
+    description VARCHAR(1000) NOT NULL,
+    sum NUMERIC(2) NOT NULL,
+    created_date DATE NOT NULL,
+    picture TEXT,
+    user_id BIGINT NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+
+CREATE TABLE comments
+(
+	id BIGSERIAL PRIMARY KEY,
+	text VARCHAR(1000) NOT NULL,
+	created_date DATE NOT NULL,
+	offer_id BIGINT NOT NULL,
+	user_id BIGINT NOT NULL,
+  FOREIGN KEY (offer_id) REFERENCES offers
+	ON UPDATE CASCADE
+	ON DELETE CASCADE,
+  FOREIGN KEY (user_id) REFERENCES users
+	ON UPDATE CASCADE
+	ON DELETE CASCADE
+);
+
+
+CREATE TABLE categories
+(
+	id BIGSERIAL PRIMARY KEY,
+	title VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE offers_categories
+(
+  offer_id BIGINT,
+  category_id BIGINT,
+  CONSTRAINT offers_categories_pk PRIMARY KEY(offer_id, category_id),
+  FOREIGN KEY(offer_id) REFERENCES offers
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY(category_id) REFERENCES categories
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
+
+CREATE TABLE types
+(
+	id BIGSERIAL PRIMARY KEY,
+	title VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE offers_types
+(
+  offer_id BIGINT,
+  type_id BIGINT,
+  CONSTRAINT offers_types_pk PRIMARY KEY(offer_id, type_id),
+  FOREIGN KEY(offer_id) REFERENCES offers
+    ON UPDATE CASCADE
+    ON DELETE CASCADE,
+  FOREIGN KEY(type_id) REFERENCES types
+    ON UPDATE CASCADE
+    ON DELETE CASCADE
+);
